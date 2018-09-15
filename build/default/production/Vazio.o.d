5d49ee82e368eb3b3f7a36e9d70b44cb57ba0b60
Suanny Fabyne <suanny_fabyne@hotmail.com>
2018-09-14 21:22:16 -0300

    Interrupt's uses

---
+++ build/default/production/Vazio.o.d
@@ -0,0 +1,4 @@
+build/default/production/Vazio.o:  \
+	/home/suannyfabyne/Documentos/P6/Microcontroladores/MPLab_Projects/piscapisca.X/Vazio.ASM  \
+	/opt/microchip/mplabx/v4.10/mpasmx/p12f675.inc  \
+
